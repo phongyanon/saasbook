620c1fe58eec4f82251ff46f62c7f097a374b90f
saasbook <saasbook@saasbook.(none)>
2014-09-17 12:50:41 -0300

    check error by myself in movies_controller.rb, show.html.haml and index.html.haml and then I've fix them already

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Myrottenpotatoes::Application.routes.draw do
 	resources :movies
 	root :to => redirect('/movies')
-end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
