051247da8bb2077aaff1c9e4fd94de72d49c6387
saasbook <saasbook@saasbook.(none)>
2014-09-17 11:03:11 -0300

    delete MoviesController and add show() in movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
-class MoviesController < ApplicationController
-	def index
-		@movies = Movie.all
-	end
+#class MoviesController < ApplicationController
+#	def index
+#		@movies = Movie.all
+#	end
+#end
+
+def show
+	id = params[:id]
+	@movies = Movies.find(id)
 end
